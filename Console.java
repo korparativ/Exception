98669ee4107591f3e141545c40c8a9eb6ad5079f
Aleks <korparativ@mail.ru>
2024-06-15 20:51:54 +1200

    first commit

---
+++ Console.java
@@ -0,0 +1,5 @@
+package Exception.Exception;
+
+public class Console {
+    System.out.println ("Введите следующие данные через пробел: Фамилия Имя Отчество Дата_рождения(dd.mm.yyyy) Номер телефона(целое беззнаковое число) Пол(символ латиницей f или m)");
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+package Exception.Exception;
+
+public class Main {
+   // System.out.println();
+}
